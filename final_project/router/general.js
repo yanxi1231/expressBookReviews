7e69b240e7e691338443a03bf489c8ec966e4026
yanxi1231 <na.ya@northeastern.edu>
2025-08-20 12:04:37 -0400

    add general

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -51,10 +51,23 @@ public_users.get('/author/:author', function (req, res) {
 
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title.trim().toLowerCase();
+    const matchingBooks = [];
+
+    for (const [key, book] of Object.entries(books)) {
+        if (book.title.toLowerCase().includes(title)) {
+            matchingBooks.push({ isbn: key, ...book });
+        }
+    }
+
+    if (matchingBooks.length > 0) {
+        return res.status(200).json(matchingBooks);
+    } else {
+        return res.status(404).json({ message: "No books found matching this title." });
+    }
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
